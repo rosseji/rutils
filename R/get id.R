3b621eab90aa4ace4a379cfea606c56e67024fb9
rosseji <rosseireland@gmail.com>
2018-03-04 21:42:27 +1100

    add more

--- R/get id.R
+++ R/get id.R
@@ -2,7 +2,7 @@
 #' @export
 
 get_id <- function() {
-  paste0(sample(LETTERS, 1), Sys.time() %>% as.numeric()) %>% str_replace("\\.", "")
+  paste0(sample(LETTERS, 1), Sys.time() %>% as.numeric() %>% as.character()) %>% str_replace("\\.", "")
 }
 
 
