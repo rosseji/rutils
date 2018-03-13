40b8201ac594bbf169a0a5d292bd7fd322874c72
rosseji <rosseireland@gmail.com>
2018-03-14 10:41:24 +1100

    update id creator

--- R/get id.R
+++ R/get id.R
@@ -2,7 +2,7 @@
 #' @export
 
 get_id <- function() {
-  paste0(sample(LETTERS, 1), Sys.time() %>% as.numeric() %>% as.character()) %>% str_replace("\\.", "")
+  paste0(sample(LETTERS, 2) %>% str_c(collapse = ""), Sys.time() %>% as.numeric() %>% as.character()) %>% str_replace("\\.", "")
 }
 
 
