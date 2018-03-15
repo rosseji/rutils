5a0756ce60a28b99beb34a4cecf2a4ccb0c29659
rosseji <rosseireland@gmail.com>
2018-03-16 10:58:30 +1100

    add roman francois cool lags fun

---
+++ R/lags.R
@@ -0,0 +1,12 @@
+
+
+#' @export
+
+lags <- function(var, n=10){
+  var <- enquo(var)
+
+  indices <- seq_len(n)
+  map( indices, ~quo(lag(!!var, !!.x)) ) %>%
+    set_names(sprintf("lag_%s_%02d", rlang::quo_text(var), indices))
+}
+
