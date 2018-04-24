e6f879531c2efbad3aaa94ba9f00ba4497118ab4
rosseji <rosseireland@gmail.com>
2018-04-24 11:06:15 +1000

    revert to og

--- R/lags.R
+++ R/lags.R
@@ -2,16 +2,9 @@
 
 #' @export
 
-lags <- function(var, n = 10, .keep_all = T){
-
-  if (.keep_all == F) {
-    indices <- seq(n)[-1]
-  } else {
-    indices <- seq(n)
-  }
-
+lags <- function(var, n = 10){
+  indices <- seq(n)
   map( indices, ~ quo(lag(!!rlang::sym(var), !!.x)) ) %>%
-    set_names(sprintf("lag_%s_%02d", var, indices))%>%
-    select(-rlang::sym(var))
+    set_names(sprintf("lag_%s_%02d", var, indices))
 }
 
--- R/leads.R
+++ R/leads.R
@@ -1,17 +1,9 @@
 
 #' @export
 
-leads <- function(var, n = 10, .keep_all = T){
-
-  if (.keep_all == F) {
-    indices <- seq(n)[-1]
-  } else {
-    indices <- seq(n)
-  }
-
-  map( indices, ~ quo(lead(!!rlang::sym(var), !!.x)) ) %>%
-    set_names(sprintf("lead_%s_%02d", var, indices)) %>%
-    ungroup() %>%
-    select(-rlang::sym(var))
+leads <- function(var, n = 10){
+  indices <- seq(n)
+  map(indices, ~ quo(lead(!!rlang::sym(var), !!.x)) ) %>%
+    set_names(sprintf("lead_%s_%02d", var, indices))
 }
 
